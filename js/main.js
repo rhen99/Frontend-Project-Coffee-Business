7466f5dffe5e7f4b394a504f4695ecf12b3fddf1
Arhen Santiago <lincoldarhen9900@gmail.com>
2020-04-29 18:34:53 +0800

    working on mobile

--- js/main.js
+++ js/main.js
@@ -1,12 +1,52 @@
+const searchBtn = document.querySelector(".navbar__list__searchbtn");
+const searchCloseBtn = document.querySelector(".search__div__close");
+const newsletterText = document.querySelector(".newsletter__text");
+
+searchBtn.addEventListener("click", openSearchBox);
+searchCloseBtn.addEventListener("click", closeSearchBox);
+newsletterText.addEventListener("click", showNewsletterForm);
+
+function openSearchBox(e) {
+  e.preventDefault();
+  gsap
+    .timeline()
+    .to(".search__outer", 0.2, {
+      display: "block",
+      opacity: "1",
+    })
+    .to(".search__div", 0.2, {
+      transform: "translate3d(0,0,0)",
+    });
+}
+function closeSearchBox(e) {
+  e.preventDefault();
+  gsap
+    .timeline()
+    .to(".search__div", 0.2, {
+      transform: "translate3d(0, -100%, 0)",
+    })
+    .to(".search__outer", 0.2, {
+      opacity: "0",
+      display: "none",
+    });
+}
+function showNewsletterForm(e) {
+  e.preventDefault();
+  gsap.timeline().to(".newsletter__container", 0.3, {
+    transform: "translate3d(0, -70px, 0)",
+  });
+}
+
+//Navbar Shrink
 const controller = new ScrollMagic.Controller();
 const amimateNavbar = gsap
   .timeline()
-  .from(".navbar", {
-    backgroundColor: "transparent",
-    padding: "1rem",
+  .to(".navbar", {
+    backgroundColor: "#000",
+    padding: "0",
   })
-  .from(".navbar__list__link", {
-    fontSize: "1.1rem",
+  .to(".navbar__list__link", {
+    fontSize: "1rem",
   });
 new ScrollMagic.Scene({
   triggerElement: ".showcase",
@@ -15,3 +55,4 @@ new ScrollMagic.Scene({
 })
   .setTween(amimateNavbar)
   .addTo(controller);
+const scroll = new SmoothScroll('.showcase__inner__action[href*="#"]');
